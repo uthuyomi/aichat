f4f9221cc9d25a7695a1acbf54de443b0a84ff1b
uthuyomi <kaiseif4e@gmail.com>
2026-08-26 20:03:13 +0900

    auto-commit

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,10 +1,14 @@
 import { Thread } from "@/components/thread";
 import MyRuntimeProvider from "./MyRuntimeProvider";
+import  sidebar from ""
 
 export default function Home() {
   return (
     <MyRuntimeProvider>
+      <div className="flex hscreen">
+        <Sidebar />
+      </div>
       <Thread />
     </MyRuntimeProvider>
-  )
+  );
 }

5870085a429cab0029e087b4989f3e44f581aef1
uthuyomi <kaiseif4e@gmail.com>
2026-08-29 07:52:11 +0900

    Initial commit

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,14 +1,20 @@
 import { Thread } from "@/components/thread";
 import MyRuntimeProvider from "./MyRuntimeProvider";
-import  sidebar from ""
+import { ThreadListSidebar } from "@/components/threadlist-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Home() {
   return (
     <MyRuntimeProvider>
-      <div className="flex hscreen">
-        <Sidebar />
-      </div>
-      <Thread />
+      <SidebarProvider>
+        <ThreadListSidebar />
+        <main className="flex min-w-0 flex-1 flex-col">
+          <SidebarTrigger />
+          <div className="min-h-0 flex-1">
+            <Thread />
+          </div>
+        </main>
+      </SidebarProvider>
     </MyRuntimeProvider>
   );
 }

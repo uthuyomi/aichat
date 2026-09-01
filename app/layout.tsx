0ed82235b944bce17585a40d47d9a27c614f5be5
uthuyomi <kaiseif4e@gmail.com>
2026-09-01 21:37:25 +0900

    auto-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,17 +23,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider 
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        >
-          <OpenUiProvider>
-            {children}
-          </OpenUiProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <OpenUiProvider>{children}</OpenUiProvider>
         </ThemeProvider>
       </body>
     </html>
